318d7c8e71d3e8dbdcb6bbf24ef660d382bc4dfe
Francesco Refolli <f.refolli@campus.unimib.it>
2026-02-14 11:16:40 +0100

    We got MUMs oh yeah

--- mum/README.md
+++ mum/README.md
@@ -1,45 +1,92 @@
 ## Alphabet
-$\Sigma$ := (Characters = [a, c, g, t], Terminal = `$`, Separator = `#`)
+$\Sigma$ := (Characters = [a, b, c, d], Terminal = `$`, Separator = `#`)
 
+
+## TextS
+$T_1$ := abcdaaaa$
+
+$T_2$ := bbbabcd$
 
 ## Text
-T := acaaacatat$
+T := abcdaaaa#bbbabcd$
 
+## Local Maximums
+### [5,6]
+lcp := 3
+match := `aaa`
+### [7,8]
+lcp := 4
+match := `abcd`
+### [10,11]
+lcp := 2
+match := `bb`
+### [12,13]
+lcp := 3
+match := `bcd`
+### [14,15]
+lcp := 2
+match := `cd`
+## Global Maximums
+### [5,6]
+lcp := 3
+match := `aaa`
+### [7,8]
+lcp := 4
+match := `abcd`
+### [10,11]
+lcp := 2
+match := `bb`
+## Maximal Unique Matches
+### [7,8]
+lcp := 4
+match := `abcd`
 ## Suffix Array, LCP and BWT
-| SA  | LCP |  B  | SUFFIX |
-| --- | --- | --- | ------ |
-|11|-1|t|`$`|
-|3|0|c|`aaacatat$`|
-|4|2|a|`aacatat$`|
-|1|1|$|`acaaacatat$`|
-|5|3|a|`acatat$`|
-|9|1|t|`at$`|
-|7|2|c|`atat$`|
-|2|0|a|`caaacatat$`|
-|6|2|a|`catat$`|
-|10|0|a|`t$`|
-|8|1|a|`tat$`|
+| `#` | SA  | LCP |  B  | SUFFIX |
+| --- | --- | --- | --- | ------ |
+|1|17|-1|d|`$`|
+|2|9|0|a|`#bbbabcd$`|
+|3|8|0|a|`a#bbbabcd$`|
+|4|7|1|a|`aa#bbbabcd$`|
+|5|6|2|a|`aaa#bbbabcd$`|
+|6|5|3|d|`aaaa#bbbabcd$`|
+|7|13|1|b|`abcd$`|
+|8|1|4|$|`abcdaaaa#bbbabcd$`|
+|9|12|0|b|`babcd$`|
+|10|11|1|b|`bbabcd$`|
+|11|10|2|#|`bbbabcd$`|
+|12|14|1|a|`bcd$`|
+|13|2|3|a|`bcdaaaa#bbbabcd$`|
+|14|15|0|b|`cd$`|
+|15|3|2|b|`cdaaaa#bbbabcd$`|
+|16|16|0|c|`d$`|
+|17|4|1|c|`daaaa#bbbabcd$`|
 
 ## FM Index
 | $\sigma$ | $C[\sigma]$ |
 | -------- | ----------- |
 |$|0|
 |#|1|
-|a|1|
-|c|7|
-|g|9|
-|t|9|
+|a|2|
+|b|8|
+|c|13|
+|d|15|
 
-| $i$ | $Occ[$`$`$]$ | $Occ[$`#`$]$ | $Occ[$`a`$]$ | $Occ[$`c`$]$ | $Occ[$`g`$]$ | $Occ[$`t`$]$ |
+| $i$ | $Occ[$`$`$]$ | $Occ[$`#`$]$ | $Occ[$`a`$]$ | $Occ[$`b`$]$ | $Occ[$`c`$]$ | $Occ[$`d`$]$ |
 | --- | -------- | -------- | -------- | -------- | -------- | -------- |
 | 1 | 0| 0| 0| 0| 0| 0|
 | 2 | 0| 0| 0| 0| 0| 1|
-| 3 | 0| 0| 0| 1| 0| 1|
-| 4 | 0| 0| 1| 1| 0| 1|
-| 5 | 1| 0| 1| 1| 0| 1|
-| 6 | 1| 0| 2| 1| 0| 1|
-| 7 | 1| 0| 2| 1| 0| 2|
-| 8 | 1| 0| 2| 2| 0| 2|
-| 9 | 1| 0| 3| 2| 0| 2|
+| 3 | 0| 0| 1| 0| 0| 1|
+| 4 | 0| 0| 2| 0| 0| 1|
+| 5 | 0| 0| 3| 0| 0| 1|
+| 6 | 0| 0| 4| 0| 0| 1|
+| 7 | 0| 0| 4| 0| 0| 2|
+| 8 | 0| 0| 4| 1| 0| 2|
+| 9 | 1| 0| 4| 1| 0| 2|
 | 10 | 1| 0| 4| 2| 0| 2|
-| 11 | 1| 0| 5| 2| 0| 2|
+| 11 | 1| 0| 4| 3| 0| 2|
+| 12 | 1| 1| 4| 3| 0| 2|
+| 13 | 1| 1| 5| 3| 0| 2|
+| 14 | 1| 1| 6| 3| 0| 2|
+| 15 | 1| 1| 6| 4| 0| 2|
+| 16 | 1| 1| 6| 5| 0| 2|
+| 17 | 1| 1| 6| 5| 1| 2|
--- mum/src/main.cc
+++ mum/src/main.cc
@@ -112,12 +112,35 @@ struct Text {
     return out;
   }
 
-  std::string slice_suffix(const Alphabet& alphabet, uintmax_t start_idx) {
+  std::string slice_suffix(const Alphabet& alphabet, uintmax_t start_idx) const {
     std::string result = "";
     for (uintmax_t idx = start_idx; idx < symbols.size(); ++idx) {
       result += alphabet.symbols[symbols[idx]];
     }
     return result;
+  }
+
+  inline Text invert() const {
+    Text text;
+    text.symbols.resize(this->symbols.size());
+    for (uintmax_t idx = 0; idx < this->symbols.size() - 1; ++idx) {
+      text.symbols[text.symbols.size() - 2 - idx] = this->symbols[idx];
+    }
+    text.symbols[text.symbols.size() - 1] = this->symbols.back();
+    return text;
+  }
+
+  inline Text join(const Text& other, const Alphabet& alphabet) const {
+    Text text;
+    text.symbols.resize(this->symbols.size() + other.symbols.size());
+    for (uintmax_t idx = 0; idx < this->symbols.size(); ++idx) {
+      text.symbols[idx] = this->symbols[idx];
+    }
+    text.symbols[this->symbols.size() - 1] = alphabet.separator();
+    for (uintmax_t idx = 0; idx < other.symbols.size(); ++idx) {
+      text.symbols[idx + this->symbols.size()] = other.symbols[idx];
+    }
+    return text;
   }
 };
 
@@ -288,12 +311,171 @@ std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
   return out;
 }
 
+struct LocalMaximumIterator {
+  const LCPArray& lcp_array;
+  uintmax_t q;
+  intmax_t i, j;
+
+  LocalMaximumIterator(const LCPArray& lcp_array) : lcp_array(lcp_array) {
+    this->q = 1;
+    this->i = -1;
+    this->j = -1;
+  }
+  
+  inline bool has_next() {
+    uintmax_t length = lcp_array.lcps.size();
+    i = -1;
+    j = -1;
+    while (q < length) {
+      if (lcp_array.lcps[q - 1] < lcp_array.lcps[q]) {
+        i = q - 1;
+        j = -1;
+      } else if (lcp_array.lcps[q - 1] > lcp_array.lcps[q]) {
+        j = q - 1;
+      }
+
+      if (i != -1 && j != -1) {
+        break;
+      }
+      q++;
+    }
+    return (i != -1 && j != -1);
+  }
+
+  std::pair<uintmax_t, uintmax_t> next() {
+    assert(i != -1 && j != -1);
+    std::pair<uintmax_t, uintmax_t> res = {i, j};
+    i = -1;
+    j = -1;
+    return res;
+  }
+};
+
+struct GlobalMaximumIterator {
+  LocalMaximumIterator local_maximum_iterator;
+  BurrowsWheelerTransform& burrows_wheeler_transform;
+  intmax_t i, j;
+
+  GlobalMaximumIterator(const LCPArray& lcp_array,
+                        BurrowsWheelerTransform& burrows_wheeler_transform)
+    : local_maximum_iterator(lcp_array),
+      burrows_wheeler_transform(burrows_wheeler_transform) {
+    this->i = -1;
+    this->j = -1;
+  }
+
+  inline bool has_next() {
+    this->i = -1;
+    this->j = -1;
+    if (!local_maximum_iterator.has_next()) {
+      return false;
+    }
+    auto local_maximum = local_maximum_iterator.next();
+    uintmax_t ci = local_maximum.first;
+    uintmax_t cj = local_maximum.second;
+
+    bool ok = true;
+    for (uintmax_t a = ci; a <= cj; ++a) {
+      for (uintmax_t b = a + 1; b <= cj; ++b) {
+        assert(a != b);
+        if (burrows_wheeler_transform.symbols[a] == burrows_wheeler_transform.symbols[b]) {
+          ok = false;
+        }
+
+        if (!ok) {
+          break;
+        }
+      }
+
+      if (!ok) {
+        break;
+      }
+    }
+
+    if (!ok) {
+      i = -1;
+      j = -1;
+    } else {
+      i = ci;
+      j = cj;
+    }
+
+    return ok;
+  }
+
+  std::pair<uintmax_t, uintmax_t> next() {
+    assert(i != -1 && j != -1);
+    std::pair<uintmax_t, uintmax_t> res = {i, j};
+    i = -1;
+    j = -1;
+    return res;
+  }
+};
+
+struct MaximalUniqueMatchesIterator {
+  GlobalMaximumIterator global_maximum_iterator;
+  const SuffixArray& suffix_array;
+  uintmax_t separator_position;
+  intmax_t i, j;
+
+  MaximalUniqueMatchesIterator(const LCPArray& lcp_array,
+                               BurrowsWheelerTransform& burrows_wheeler_transform,
+                               const SuffixArray& suffix_array,
+                               uintmax_t separator_position)
+    : global_maximum_iterator(lcp_array, burrows_wheeler_transform),
+      suffix_array(suffix_array) {
+    this->separator_position = separator_position;
+    this->i = -1;
+    this->j = -1;
+  }
+
+  bool has_next() {
+    this->i = -1;
+    this->j = -1;
+
+    bool ok = false;
+    while (global_maximum_iterator.has_next()) {
+      auto global_maximum = global_maximum_iterator.next();
+      if (global_maximum.second != global_maximum.first + 1) {
+        continue;
+      }
+      uintmax_t ci = global_maximum.first;
+      uintmax_t cj = global_maximum.second;
+
+      uintmax_t min_ij = std::min(suffix_array.indexes[ci], suffix_array.indexes[cj]);
+      uintmax_t max_ij = std::max(suffix_array.indexes[ci], suffix_array.indexes[cj]);
+
+      if (min_ij < separator_position && max_ij > separator_position) {
+        i = ci;
+        j = cj;
+        ok = true;
+        break;;
+      }
+    }
+
+    return ok;
+  }
+
+  std::pair<uintmax_t, uintmax_t> next() {
+    assert(i != -1 && j != -1);
+    std::pair<uintmax_t, uintmax_t> res = {i, j};
+    i = -1;
+    j = -1;
+    return res;
+  }
+};
+
 int main() {
   std::srand(170701);
   // Alphabet alphabet = Alphabet("acgt", '$', '#');
   // Text text = Text::from(alphabet, "ggtcagtc$");
-  Alphabet alphabet = Alphabet("acgt", '$', '#');
-  Text text = Text::from(alphabet, "acaaacatat$");
+  // Alphabet alphabet = Alphabet("acgt", '$', '#');
+  // Text t1 = Text::from(alphabet, "acaaacatat$");
+  // Text t2 = t1.invert();
+  Alphabet alphabet = Alphabet("abcd", '$', '#');
+  Text t1 = Text::from(alphabet, "abcdaaaa$");
+  Text t2 = Text::from(alphabet, "bbbabcd$");
+  Text text = t1.join(t2, alphabet);
   SuffixArray suffix_array = SuffixArray::build(text);
   BurrowsWheelerTransform burrows_wheeler_transform = BurrowsWheelerTransform::build(text, suffix_array);
   FMIndex fm_index = FMIndex::build(alphabet, burrows_wheeler_transform);
@@ -306,15 +488,55 @@ int main() {
   file << std::endl;
   file << std::endl;
 
+  file << "## TextS" << std::endl;
+  t1.to_markdown(file << "$T_1$ := ", alphabet) << std::endl;
+  file << std::endl;
+  t2.to_markdown(file << "$T_2$ := ", alphabet) << std::endl;
+  file << std::endl;
+
   file << "## Text" << std::endl;
   text.to_markdown(file << "T := ", alphabet) << std::endl;
   file << std::endl;
+
+  file << "## Local Maximums" << std::endl;
+  LocalMaximumIterator local_maximum_iterator = LocalMaximumIterator(lcp_array);
+  while (local_maximum_iterator.has_next()) {
+    auto ij = local_maximum_iterator.next();
+    file << "### [" << ij.first+1 << "," << ij.second+1 << "]" << std::endl;
+    uintmax_t lcp = lcp_array.lcps[ij.first + 1];
+    std::string match = text.slice_suffix(alphabet, suffix_array.indexes[ij.first + 1]).substr(0, lcp);
+    file << "lcp := " << lcp << std::endl;
+    file << "match := `" << match << "`" << std::endl;
+  }
+
+  file << "## Global Maximums" << std::endl;
+  GlobalMaximumIterator global_maximum_iterator = GlobalMaximumIterator(lcp_array, burrows_wheeler_transform);
+  while (global_maximum_iterator.has_next()) {
+    auto ij = global_maximum_iterator.next();
+    file << "### [" << ij.first+1 << "," << ij.second+1 << "]" << std::endl;
+    uintmax_t lcp = lcp_array.lcps[ij.first + 1];
+    std::string match = text.slice_suffix(alphabet, suffix_array.indexes[ij.first + 1]).substr(0, lcp);
+    file << "lcp := " << lcp << std::endl;
+    file << "match := `" << match << "`" << std::endl;
+  }
+
+  file << "## Maximal Unique Matches" << std::endl;
+  MaximalUniqueMatchesIterator maximal_unique_matches_iterator = MaximalUniqueMatchesIterator(lcp_array, burrows_wheeler_transform, suffix_array, t1.size());
+  while (maximal_unique_matches_iterator.has_next()) {
+    auto ij = maximal_unique_matches_iterator.next();
+    file << "### [" << ij.first+1 << "," << ij.second+1 << "]" << std::endl;
+    uintmax_t lcp = lcp_array.lcps[ij.first + 1];
+    std::string match = text.slice_suffix(alphabet, suffix_array.indexes[ij.first + 1]).substr(0, lcp);
+    file << "lcp := " << lcp << std::endl;
+    file << "match := `" << match << "`" << std::endl;
+  }
   
   file << "## Suffix Array, LCP and BWT" << std::endl;
-  file << "| SA  | LCP |  B  | SUFFIX |" << std::endl;
-  file << "| --- | --- | --- | ------ |" << std::endl;
+  file << "| `#` | SA  | LCP |  B  | SUFFIX |" << std::endl;
+  file << "| --- | --- | --- | --- | ------ |" << std::endl;
   for (uintmax_t idx = 0; idx < text.size(); ++idx) {
     file
+      << "|" << idx+1
       << "|" << suffix_array.indexes[idx] + 1
       << "|" << (intmax_t)(lcp_array.lcps[idx])
       << "|"  << alphabet.symbols[burrows_wheeler_transform.symbols[idx]]
